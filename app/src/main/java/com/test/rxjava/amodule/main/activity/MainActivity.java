769e6586ef9ed9bcdb86d4809075b5034606d40c
duhaoyuan <fosung@DESKTOP-267LB77>
2017-04-13 15:27:57 +0800

    **测试

--- app/src/main/java/com/test/rxjava/amodule/main/activity/MainActivity.java
+++ app/src/main/java/com/test/rxjava/amodule/main/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.test.rxjava;
+package com.test.rxjava.amodule.main.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.test.rxjava.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
